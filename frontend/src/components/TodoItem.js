181b6aaadd23e53d2c6a66c79a1b6f75c55a3b70
Aleksandar Petrov <sasho1320@gmail.com>
2023-08-22 17:06:54 +0300

    Update frontend files

--- frontend/src/components/TodoItem.js
+++ frontend/src/components/TodoItem.js
@@ -1,17 +1,21 @@
 import styles from "./TodoItem.module.css";
 
 const TodoItem = (props) => {
-    let taskStatus = "note-wrapper";
+    let taskStatus = "note-wrapper-uncompleted";
     if (props.completed) {
-        taskStatus += " completed";
+        taskStatus = "note-wrapper-completed";
     }
 
     return (
         <div className={styles[taskStatus]}>
             <h3 className={styles["note-title"]}>{props.title}</h3>
-            <p className={styles["note-desc"]}>{props.description}</p>
-            <p className={styles["note-status"]}>{props.completed ? "Complete" : "Incomplete"}</p>
-            <button className={styles["note-button"]}>Change status</button>
+            {/* <p className={styles["note-desc"]}>{props.description}</p> */}
+            <div className={styles["button-wrapper"]}>
+                <p className={styles["note-status"]}>
+                    {props.completed ? "Completed" : "Uncompleted"}
+                </p>
+                <button className={styles["note-button"]}>Change status</button>
+            </div>
         </div>
     );
 };
